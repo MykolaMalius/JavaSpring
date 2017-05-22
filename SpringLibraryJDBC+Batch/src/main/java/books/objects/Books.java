3ebac514253fdcd4ffe4a0c8e2b8501ed2433386
MykolaMalius <mmalyus@mail.ru>
2017-05-22 13:53:18 +0300

    spring + jdbc - Library

---
+++ SpringLibraryJDBC+Batch/src/main/java/books/objects/Books.java
@@ -0,0 +1,39 @@
+package books.objects;
+
+
+public class Books {
+
+	private int id;
+	private String author;
+	private String book;
+	private int year;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getBook() {
+		return book;
+	}
+	public void setBook(String book) {
+		this.book = book;
+	}
+	public int getYear() {
+		return year;
+	}
+	public void setYear(int year) {
+		this.year = year;
+	}
+	
+	
+	
+}
